d9d337ee43c85c3bfef17581fab8525066375e97
Unknown <lizsarni@gmail.com>
2018-11-24 15:26:01 -0800

    Contact Address

--- contact.php
+++ contact.php
@@ -37,7 +37,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@themeforest.net";
-$address = "fgpatrick2002@yahoo.com";
+$address = "lizsarni@gmail.com";
 
 
 // Configuration option.
